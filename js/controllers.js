58ccd387bd3cd7227a18351ec161ddf428f587b5
Pete Field <pedalpete@hotmail.com>
2013-08-29 18:10:57 +1000

    showing other search results from app store, files and bing

--- js/controllers.js
+++ js/controllers.js
@@ -61,42 +61,6 @@ app.controller('AppListCtrl',['$scope',function($scope){
 
 
 	}]);
-app.filter('partition', function() {
-  var part = function(arr, size) {
-    if ( 0 === arr.length ) return [];
-    var applist=[];
-    var partlist=[];
-    var blocksize=0;
-    var emptyapp={"name":"","width":"single"}
-    console.log(arr);
-    for(var a in arr){
-    	var app = arr[a];
-
-    	if(app.width=='single'){
-    		console.log(app.name);
-    		partlist.push(app);
-    		blocksize++;
-    	}
-    	if(app.width=='double' && blocksize<4){
-    		partlist.push(app);
-    	}
-    	if(app.width=='double' && blocksize>=4){
-    		applist.push(partlist);
-    		partlist=[app];
-    		blocksize=2;
-    	}
-   
-    	if(blocksize==size || a==arr.length-1){
-    			applist.push(partlist);
-    			partlist=[];
-    			blocksize=0;
-    		}
-    }
-    console.log(applist)
-   return applist;
-  };
-  return part;
-});
 
 app.directive('search-input', function () {
     return function (scope, element, attrs) {
